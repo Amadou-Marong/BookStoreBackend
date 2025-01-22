3ccaebb5c030d4ca53f0510759ff850e7866e221
amadou.marong <marongamadou6@gmail.com>
2025-01-22 21:50:13 +0000

    added backend mongodb uri

--- config.js
+++ config.js
@@ -1,2 +1,3 @@
 export const PORT = 5555;
-export const MONGO_URI = 'mongodb://localhost:27017/bookStore';
+// export const MONGO_URI = 'mongodb://localhost:27017/bookStore';
+export const MONGO_URI = 'mongodb+srv://root:1234@cluster-1.txhym.mongodb.net/BookStore';
--- index.js
+++ index.js
@@ -10,13 +10,14 @@ import { fileURLToPath } from 'url';
 const upload = multer({ dest: 'uploads/' });
 
 const app = express();
-app.use(cors(
-    {
-        origin: 'http://localhost:5173',
-        methods: ['GET', 'POST', 'PUT', 'DELETE'],
-        credentials: true,
-    }
-));
+// app.use(cors(
+//     {
+//         origin: 'http://localhost:5173',
+//         methods: ['GET', 'POST', 'PUT', 'DELETE'],
+//         credentials: true,
+//     }
+// ));
+app.use(cors());
 app.use(express.json());
 
 // Properly resolve __dirname in ES modules
